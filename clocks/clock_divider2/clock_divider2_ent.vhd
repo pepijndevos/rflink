98ddca40a378d45360a5e787c65bd42ab36e01bb
olafvdm <o.r.c.vandermeer@gmail.com>
2018-12-17 16:12:26 +0100

    transmitter v3 compiled

---
+++ clocks/clock_divider2/clock_divider2_ent.vhd
@@ -0,0 +1,11 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+entity clock_divider2 is
+  generic (clk_div: integer := 10);
+  port(clk_high_freq    : in std_logic;
+       reset 		: in std_logic;
+       clk_low_freq     : out std_logic
+       );
+end clock_divider2;
+
