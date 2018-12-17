461e39836a0767eacd4b589835277d486ee4d077
olafvdm <o.r.c.vandermeer@gmail.com>
2018-12-17 11:17:31 +0100

    New transceiver v0
    For testing, we make a version 0 transceiver

---
+++ transceiver/transceiver v0/transmitter/audiobuffer_ent.vhd
@@ -0,0 +1,12 @@
+library IEEE;  
+use IEEE.STD_LOGIC_1164.ALL;  
+use IEEE.NUMERIC_STD.ALL;
+
+entity audiobuffer is
+  generic(word_length: integer:= 8);
+	port(
+    clk: in std_logic;
+	data_in: in std_logic_vector(word_length-1 downto 0);
+	data_out: out std_logic_vector(word_length-1 downto 0)
+	);
+end audiobuffer;
