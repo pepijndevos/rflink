93fcc7f3bdfaf166fa621c2379125c6801548c6e
olafvdm <o.r.c.vandermeer@gmail.com>
2018-12-20 14:44:40 +0100

    finalizing transceiver v3

--- p_2_s and s_2_p/p_2_s/p_2_s_arch.vhd
+++ p_2_s and s_2_p/p_2_s/p_2_s_arch.vhd
@@ -8,18 +8,18 @@ architecture behavioral of p_2_s is
   signal counter_unbuffer: integer range 0 to (word_length_unbuffer-1);
 
 begin
-  -- the next process is sequential and only sensitive to clk and reset
---  seq_parallel: process(clk_unbuffer_parallel, reset)
---  begin
---    if (reset = '0')
---    then
---	data_in_temp1 <= (others =>'0');
---    elsif rising_edge(clk_unbuffer_parallel)
---    then
---	data_in_temp1 <= data_in_unbuffer;
---	--data_out_temp <= data_in_temp(0);
---    end if; -- (reset = '0')
---  end process seq_parallel;
+-- the next process is sequential and only sensitive to clk and reset
+  seq_parallel: process(clk_unbuffer_parallel, reset)
+  begin
+    if (reset = '0')
+    then
+	data_in_temp1 <= (others =>'0');
+    elsif rising_edge(clk_unbuffer_parallel)
+    then
+data_in_temp1 <= data_in_unbuffer;
+	--data_out_temp <= data_in_temp(0);
+    end if; -- (reset = '0')
+  end process seq_parallel;
 
   seq_serial: process(clk_unbuffer_serial, reset)
   begin
@@ -32,7 +32,7 @@ begin
     then
 	if(counter_unbuffer = 0)
 	then
-	    data_in_temp2 <= '0'&data_in_unbuffer(word_length_unbuffer-1 downto 1);
+	    data_in_temp2 <= '0'&data_in_temp1(word_length_unbuffer-1 downto 1);
 	    counter_unbuffer <= counter_unbuffer+1;
 	    data_out_temp <= data_in_temp1(0);
 	elsif(counter_unbuffer = (word_length_unbuffer-1))
