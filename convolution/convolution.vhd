2ba41c928704aa0009707fd03da858037aaec5ee
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-12-21 12:47:12 +0100

    Near to a fix on the overflow issue of convolution

--- convolution/convolution.vhd
+++ convolution/convolution.vhd
@@ -18,44 +18,45 @@ end entity;
 
 architecture Behavorial of convolution is
 
-	type buff_type is array(0 to 19) of std_logic_vector(20-1 downto 0);
+	type buff_type is array(0 to 19) of std_logic_vector(word_length-1 downto 0);
 	type filter_type is array(0 to 19) of std_logic_vector(word_length-1 downto 0);
 	signal buff : buff_type;
 	signal signal_out_tmp : std_logic_vector(10-1 downto 0);
+	signal mult : std_logic_vector(20-1 downto 0);
  
 	signal filter_low : filter_type :=
 	(  
 		0 => "0000000000",
-		1 => "1111111111",
-		2 => "0000001101",
-		3 => "0000000101",
-		4 => "1111101101",
-		5 => "1111111001",
-		6 => "0000000101",
-		7 => "1111110111",
-		8 => "0000101111",
-		9 => "0010011111",
-		10 => "0010011111",
-		11 => "0000101111",
-		12 => "1111110111",
-		13 => "0000000101",
-		14 => "1111111001",
-		15 => "1111101101",
-		16 => "0000000101",
-		17 => "0000001101",
-		18 => "1111111111",
-		19 => "0000000000"
+		1 => "1110010000",
+		2 => "1110010010",
+		3 => "1111111001",
+		4 => "0001000010",
+		5 => "0000101011",
+		6 => "0000000001",
+		7 => "0000100000",
+		8 => "0001010100",
+		9 => "0000011010",
+		10 => "1101110111",
+		11 => "1100101001",
+		12 => "1110111111",
+		13 => "0010111110",
+		14 => "0100001011",
+		15 => "0001010001",
+		16 => "1101110101",
+		17 => "1101101111",
+		18 => "1111101110",
+		19 => "1111001001"
 	);
 
 begin
-
 	Filter_process: process(clk, reset)
 	begin
 		if(reset='0') then
 			buff <= (others =>(others => '0'));
 		elsif(rising_edge(clk)) then
 			for i in 0 to 20-1 loop
-				buff(i) <= signed(buff(i)) + signed(signal_in) * signed(filter_low(i));
+				mult <= (signed(signal_in) * signed(filter_low(i)));
+				buff(i) <= signed(buff(i)) + signed(mult(20-1 downto 10));
 			end loop;
 			signal_out_tmp<=buff(0)(word_length-1 downto 0);
 			
