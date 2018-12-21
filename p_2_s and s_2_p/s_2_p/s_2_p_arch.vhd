74419523552c490879439dc5f801e2f8b5ece2f4
Remi Jonkman <rb.jonkman@gmail.com>
2018-12-21 10:37:51 +0100

    Added decent button structure

--- p_2_s and s_2_p/s_2_p/s_2_p_arch.vhd
+++ p_2_s and s_2_p/s_2_p/s_2_p_arch.vhd
@@ -8,35 +8,38 @@ architecture behavioral of s_2_p is
   signal counter_delay : integer;
   signal counter_delay_temp : integer
 begin
-  -- the next process is sequential and only sensitive to clk and reset
-  seq_serial: process(clk_buffer_serial, reset)
-  begin
-    if (reset = '0')
-    then
-		data_out_temp1 <= (others =>'0');
-		data_in_buffer_temp0 <= '0';
-		counter_delay <= 0;
-		counter_delay_temp <= 0;
-    elsif rising_edge(clk_buffer_serial)
-    then
-		if(counter_delay_temp < 32550)
-		then
-			if (delay = '0')
-			then
-				counter_delay_temp <= counter_delay_temp + 1;
-			end if;
-		else
-			counter_delay_temp <= 0;
-			if(counter_delay  >= 9)
-			then
-				counter_delay <= 0;
-			else
+	btn: process(clk_buffer_serial, reset)
+		variable next_pressed : std_logic := '0';
+	begin
+		if (reset = '0') then
+			next_pressed := '0';
+			counter_delay <= 0;
+		elsif (rising_edge(clk_buffer_serial)) then
+			-- Next frequency
+			if (delay = '1' and next_pressed = '0') then
+				next_pressed := '1';
 				counter_delay <= counter_delay + 1;
+				
+				if (counter_delay > 9) then
+					counter_delay <= 0;
+				end if;
+			elsif (delay = '1' and next_pressed = '1') then
+				next_pressed := '1';
+			elsif (delay = '0') then
+				next_pressed := '0';
 			end if;
 		end if;
+	end process control;  
 
+	-- the next process is sequential and only sensitive to clk and reset
+  seq_serial: process(clk_buffer_serial, reset)
+  begin
+    if (reset = '0') then
+			data_out_temp1 <= (others =>'0');
+			data_in_buffer_temp0 <= '0';
+    elsif rising_edge(clk_buffer_serial) then
 		 data_out_temp3 <= data_in_buffer&data_out_temp3(word_length_buffer-1 downto 1);
-	    data_out_temp1 <= data_out_temp3(counter_delay)&data_out_temp1(word_length_buffer-1 downto 1);
+	   data_out_temp1 <= data_out_temp3(counter_delay)&data_out_temp1(word_length_buffer-1 downto 1);
 	    --data_out_temp <= data_in_temp(0);
     end if; -- (reset = '0')
   end process seq_serial;
